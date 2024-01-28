d907a0eb412ffc803581e306f0f3f82e1294b8d1
Ajay Kumar <ajaykumar.bit.1995@gmail.com>
2024-01-28 18:25:52 +0530

    FIX: winnig at 100

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ btnHold.addEventListener("click", function(){
 	if(playing){
 	scores[activePlayer] += currentScore;
 	document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-	if(scores[activePlayer] >= 20){
+	if(scores[activePlayer] >= 100){
 		playing=false
 		diceImg.classList.add("hidden");
 		document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
