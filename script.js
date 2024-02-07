55d696c96ad661ef436eb7747d778023b9bd9b4d
Ajay Kumar <ajaykumar.bit.1995@gmail.com>
2024-02-07 23:25:27 +0530

    removed static files

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-'use strict';
-
-
-
-
+// script.js
 const diceImg = document.querySelector(".dice");
 const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
@@ -22,73 +18,71 @@ score1.textContent = 0;
 
 let currentScore, activePlayer, scores, playing;
 
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
 
-const init = function() {
-	currentScore = 0;
-	activePlayer =0;
-	scores = [0, 0];
-	playing = true;
+  score0.textContent = 0;
+  score1.textContent = 0;
 
-	score0.textContent =0;
-	score1.textContent = 0;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
 
-	currentScore0.textContent = 0;
-	currentScore1.textContent = 0;
+  player0.classList.remove("player--winner");
+  player1.classList.remove("player--winner");
+  player0.classList.add("player--active");
+  player1.classList.remove("player--active");
 
-	player0.classList.remove("player--winner");
-	player1.classList.remove("player--winner");
-	player0.classList.add("player--active");
-	player1.classList.remove("player--active");
-
-	diceImg.classList.add("hidden")
-
-
-}
+  diceImg.classList.add("hidden");
+};
 
 init();
 
-diceImg.classList.add("hidden")
+diceImg.classList.add("hidden");
 
-const switchPlayer = function(){
-		document.getElementById(`current--${activePlayer}`).textContent = 0;
-		currentScore = 0;
-		activePlayer = activePlayer === 0 ? 1 : 0;
-		player0.classList.toggle("player--active");
-		player1.classList.toggle("player--active");
-}
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
+};
 
+btnRollDice.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceImg.classList.remove("hidden");
+    diceImg.src = `https://raw.githubusercontent.com/jonasschmedtmann/complete-javascript-course/master/07-Pig-Game/final/dice-${dice}.png`;
 
-btnRollDice.addEventListener("click", function(){
-	
-	if(playing) {
-	const dice = Math.trunc(Math.random()*6) + 1;
-	diceImg.classList.remove("hidden");
-	diceImg.src= `https://raw.githubusercontent.com/jonasschmedtmann/complete-javascript-course/master/07-Pig-Game/final/dice-${dice}.png`;
-
-	if(dice !== 1){
-		currentScore += dice;
-		document.getElementById(`current--${activePlayer}`).textContent = currentScore
-	
-	} else {
-		switchPlayer()
-	}
-	}
-	
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
 });
-btnHold.addEventListener("click", function(){
-
-	if(playing){
-	scores[activePlayer] += currentScore;
-	document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-	if(scores[activePlayer] >= 100){
-		playing=false
-		diceImg.classList.add("hidden");
-		document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
-		document.querySelector(`.player--${activePlayer}`).classList.remove("player--active")
-	} else {
-		switchPlayer()
-	}
-	}	
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceImg.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
 });
 
 btnNew.addEventListener("click", init);
